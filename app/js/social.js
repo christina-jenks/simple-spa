cf3ad53579ca124da93aeed707b6c231e788a1c0
christina-jenks <jenks333+christinajenks@gmail.com>
2020-04-04 17:44:33 +0200

    Was caught off guard by / in front of linkedIn profile data, glad I wrote tests. They now pass.

--- app/js/social.js
+++ app/js/social.js
@@ -1,15 +1,15 @@
 "use strict";
 
 function gitHubLink(username) {
-
+	return "https://github.com/" + username;
 }
 
 function linkedInLink(profileSlug) {
-
+	return "https://linkedin.com" + profileSlug;
 }
 
 function twitterLink(username) {
-
+	return "https://twitter.com/" + username;
 }
 
 export {gitHubLink, linkedInLink, twitterLink}
