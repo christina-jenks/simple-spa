80dd8412ef1575687c3cb02264155a5f700b26e3
christina-jenks <jenks333+christinajenks@gmail.com>
2020-04-04 18:45:39 +0200

    Stubbed out quick sample of data, and got the name, profile picture, and location elements to render correctly

--- app/js/render.js
+++ app/js/render.js
@@ -1,9 +1,40 @@
 "use strict";
 
-function createProfile(profile) {
-	let node = document.createElement('p');
-	node.appendChild(document.createTextNode(message));
-	return node;
+
+function profilePicture(profileUrl, name) {
+	
+	let img = document.createElement('img');
+	img.setAttribute('src', profileUrl);
+	img.setAttribute('alt', "portrait of " + name);
+
+	let figure = document.createElement('figure');
+	figure.appendChild(img);
+
+	return figure;
 }
+
+function name(name) {
+	let elem = document.createElement('p');
+	elem.appendChild(document.createTextNode(name));
+	return elem;
+}
+
+function office(office) {
+	let elem = document.createElement('p');
+	elem.appendChild(document.createTextNode("Office: " + office));
+	return elem;
+}
+
+function createProfile(profile) {
+
+	let root = document.createElement('section');
+
+	root.appendChild(profilePicture(profile['imagePortraitUrl'], profile['name']));
+	root.appendChild(name(profile['name']));
+	root.appendChild(office(profile['office']));
+
+	return root;
+}
+
 
 export {createProfile}
