fc52ce69e5fdfa816ada1b2fa100a5194eb8e12f
christina-jenks <jenks333+christinajenks@gmail.com>
2020-04-20 22:30:32 +0200

    Some restructuring to prep for css, and fixed issue where key on social links wasn't unique

--- webapp/src/components/Ninja.js
+++ webapp/src/components/Ninja.js
@@ -4,10 +4,10 @@ import NinjaSocials from './NinjaSocials'
 export default function Ninja(props) {
     const ninja = props && props.ninja;
     return (
-        <section key={ninja.name}>
-            <img alt={ninja.name} src={ninja.imagePortraitUrl} />
+        <section class='ninja-portrait' >
+            <img alt={ninja.name} src={ninja.imagePortraitUrl} class='portrait' />
             <div>
-                <div>
+                <div class='ninja-details' >
                     <h2>{ninja.name}</h2>
                     <h3>Office: {ninja.office || "It's a secret!"}</h3>
                 </div>
@@ -15,7 +15,7 @@ export default function Ninja(props) {
                     (() => {
                         if(ninja.gitHub || ninja.twitter || ninja.linkedIn || ninja.stackOverflow) {
                             return (
-                                <div>
+                                <div class='ninja-socials' >
                                     <NinjaSocials ninja={ninja} />
                                 </div>
                             )
--- webapp/src/components/NinjaSocials.js
+++ webapp/src/components/NinjaSocials.js
@@ -3,10 +3,26 @@ import {gitHubLink, linkedInLink, twitterLink, stackOverflowLink} from '../utils
 export default function NinjaSocials(props) {
     const socials = [];
     const ninja = props.ninja;
-    if(ninja.gitHub) socials.push(<li key={ninja.gitHub}><a href={gitHubLink(ninja.gitHub)} ><img src="/img/gitHub.png" alt="gitHub" /></a></li>)
-    if(ninja.linkedIn) socials.push(<li key={ninja.linkedIn}><a href={linkedInLink(ninja.linkedIn)} ><img src="/img/linkedIn.png" alt="linkedIn" /></a></li>)
-    if(ninja.twitter) socials.push(<li key={ninja.twitter}><a href={twitterLink(ninja.twitter)} ><img src="/img/twitter.png" alt="twitter" /></a></li>)
-    if(ninja.stackOverflow) socials.push(<li key={ninja.stackOverflow}><a href={stackOverflowLink(ninja.stackOverflow)} ><img src="/img/stackOverflow.png" alt="stackOverflow" /></a></li>)
+    if(ninja.gitHub) socials.push(<li key={'gitHub:'+ninja.gitHub}>
+        <a href={gitHubLink(ninja.gitHub)} >
+            <img src="/img/gitHub.png" alt="gitHub" className='social-icon' />
+        </a>
+    </li>)
+    if(ninja.linkedIn) socials.push(<li key={'linkedIn:'+ninja.linkedIn}>
+        <a href={linkedInLink(ninja.linkedIn)} >
+            <img src="/img/linkedIn.png" alt="linkedIn" className='social-icon'/>
+        </a>
+    </li>)
+    if(ninja.twitter) socials.push(<li key={'twitter:'+ninja.twitter}>
+        <a href={twitterLink(ninja.twitter)} >
+            <img src="/img/twitter.png" alt="twitter" className='social-icon'/>
+        </a>
+    </li>)
+    if(ninja.stackOverflow) socials.push(<li key={'stackOverflow:'+ninja.stackOverflow}>
+        <a href={stackOverflowLink(ninja.stackOverflow)} >
+            <img src="/img/stackOverflow.png" alt="stackOverflow" className='social-icon'/>
+        </a>
+    </li>)
     return(
         <ul>
             {socials}
